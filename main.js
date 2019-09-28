5f0999d30dcd93bdc5d39bfe2b380bc281e9727b
Octanium91 <octanium91@gmail.com>
2019-09-28 15:51:07 +0300

    Update

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var btn = document.querySelector(".send");
 var input = document.querySelector(".form-control");
 var resultGame = document.querySelector(".game");
+var resultPodkol = document.querySelector(".podkol");
 var rndnubmer;
 var result;
 
@@ -16,50 +17,69 @@ var result;
 //   }
 // });
 document.querySelector(".select-group").addEventListener("click", function(event) {
-    var target = event.target;
+	resultGame.style.display = "none";
+	resultPodkol.innerText = return_podeb();
     btn.removeAttribute('disabled');
 	btn.removeAttribute('title');
-})
+});
 btn.addEventListener("click", function() {
-    random();
     var userChoice = document.querySelector("input.checkbox:checked").value;
-    render(userChoice, rndnubmer);
-    resultGame.innerText = result;
+    resultGame.innerText = render(userChoice, Math.floor(Math.random() * 3));
+	resultPodkol.innerText = return_phrase_lp();
+	resultGame.style.display = "block";
 });
 
-function random() {
-    rndnubmer = Math.random();
-    if (rndnubmer < 0.34) {
-        rndnubmer = "Камень";
-    } else if (rndnubmer <= 0.67) {
-        rndnubmer = "Ножницы";
-    } else {
-        rndnubmer = "Бумага";
-    }
-}
-
-// render function
-
 function render(user, pc) {
-    if (user === pc) {
-        result = "Никто не победил! Попробуй еще раз";
-    } else if (user === "Камень") {
-        if (pc === "Ножницы") {
-            result = "Сегодня твой день! Камень победил!";
+    if (user == pc) {
+        return "Никто не победил! Попробуй еще раз";
+    } else if (user == 0) {
+        if (pc == 1) {
+            return "Сегодня твой день! Камень победил!";
         } else {
-            result = "Не повезло, дружок! Бумага победила!";
+            return "Не повезло, дружок! Бумага победила!";
         }
-    } else if (user === "Бумага") {
-        if (pc === "Камень") {
-            result = "Сегодня твой день! Бумага победила!";
+    } else if (user == 2) {
+        if (pc == 0) {
+            return "Сегодня твой день! Бумага победила!";
         } else {
-            result = "Не повезло, дружок! Ножницы победили!";
+            return "Не повезло, дружок! Ножницы победили!";
         }
-    } else if (user === "Ножницы") {
-        if (pc === "Бумага") {
-            result = "Сегодня твой день! Ножницы победили!";
+    } else if (user == 1) {
+        if (pc == 2) {
+            return "Сегодня твой день! Ножницы победили!";
         } else {
-            result = "Не повезло, дружок! Камень победил!";
+            return "Не повезло, дружок! Камень победил!";
         }
     }
-}
+};
+
+function return_podeb() {
+	let phrases = [
+	  "хм... неплохой выбор",
+	  "Уверен?",
+	  "Говно выбор",
+	  "Ты говно как и твой выбор",
+	  "Мне кажется ты продуешь :)",
+	  "Выбор хорош, но ты гей",
+	  "Гейский выбор...",
+	];
+	
+	return phrases[Math.floor(Math.random() * phrases.length)];	
+};
+
+function return_phrase_lp() {
+	let phrases = [
+	  "Сделай выбор!",
+	  "Давай сыграем еще!",
+	  "Куколд выбирай давай!",
+	  "Камень? Ножницы? или Бумага? что выберешь?",
+	  "Если не выберешь в течении 5 секунд ты ГЕЙ!",
+	  "Давай еще!",
+	  "Если не сыкун, сделаешь выбор :D",
+	];
+	
+	return phrases[Math.floor(Math.random() * phrases.length)];	
+};		
+	
+	
+	
